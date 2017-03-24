741b8e8fdb64996091defc61555c49280846162e
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-03-24 15:57:55 +0000

    Add attributes to function node

--- src/ast/function_node.h
+++ src/ast/function_node.h
@@ -1,8 +1,11 @@
-// $Id: function_node.h,v 1.4 2017/03/24 11:41:59 ist181045 Exp $ -*- c++ -*-
+// $Id: function_node.h,v 1.5 2017/03/24 15:57:45 ist181045 Exp $ -*- c++ -*-
 #ifndef __XPL_FUNCTIONNODE_H__
 #define __XPL_FUNCTIONNODE_H__
 
 #include <cdk/ast/basic_node.h>
+#include <cdk/ast/expression_node.h>
+#include <cdk/ast/identifier_node.h>
+#include <cdk/ast/sequence_node.h>
 #include <cdk/basic_type.h>
 
 namespace xpl {
@@ -11,17 +14,39 @@ namespace xpl {
    * Class for describing program nodes.
    */
   class function_node: public cdk::basic_node {
+    int _scope;
     basic_type *_type;
+    cdk::identifier_node *_name;
+    cdk::sequence_node *_arguments;
+    cdk::expression_node *_retval;
     block_node *_body;
 
   public:
-    inline function_node(int lineno, basic_type *type, block_node *body) :
-      cdk::basic_node(lineno), _type(type), _body(body) {
-    }
+    inline function_node(int lineno, int scope, basic_type *type,
+      cdk::identifier_node *name, cdk::sequence_node *arguments,
+      cdk::expression_node *retval, block_node *body) :
+      cdk::basic_node(lineno), _scope(scope), _type(type), _name(name),
+      _arguments(arguments), _retval(retval), _body(body) {}
 
   public:
+    inline const int scope() const {
+      return _scope;
+    }
+
     inline basic_type *type() {
       return _type;
+    }
+
+    inline cdk::identifier_node *name() {
+      return _name;
+    }
+
+    inline cdk::sequence_node *arguments() {
+      return _arguments;
+    }
+    
+    inline cdk::expression_node *retval() {
+      return _retval;
     }
 
     inline block_node *body() {
