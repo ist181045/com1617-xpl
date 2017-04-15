b8a68ada114aa40ec2ba16dbd725daf457e85e65
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-04-15 12:56:56 +0100

    Tweak sweep nodes, default step set in constructor if missing

--- src/ast/sweep_up_node.h
+++ src/ast/sweep_up_node.h
@@ -1,9 +1,10 @@
-// $Id: sweep_up_node.h,v 1.5 2017/04/11 22:45:15 ist181045 Exp $ -*- c++ -*-
+// $Id: sweep_up_node.h,v 1.6 2017/04/15 11:56:50 ist181045 Exp $ -*- c++ -*-
 #ifndef __XPL_SWEEPUPNODE_H__
 #define __XPL_SWEEPUPNODE_H__
 
 #include <cdk/ast/basic_node.h>
 #include <cdk/ast/expression_node.h>
+#include <cdk/ast/integer_node.h>
 #include <cdk/ast/lvalue_node.h>
 
 namespace xpl {
@@ -18,9 +19,14 @@ namespace xpl {
   public:
     inline sweep_up_node(int lineno, cdk::lvalue_node *lvalue,
         cdk::expression_node *initial, cdk::expression_node *upper,
-        cdk::expression_node *step, cdk::basic_node *block)
+        cdk::basic_node *block, cdk::expression_node *step = nullptr)
         : cdk::basic_node(lineno), _lvalue(lvalue), _initial(initial),
         _upper(upper), _step(step), _block(block) {
+      if (step == nullptr) _step = new cdk::integer_node(lineno, 1);
+    }
+
+    ~sweep_up_node() {
+      if (_step) delete _step;
     }
 
   public:
