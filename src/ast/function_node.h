6b701866a0ef0ba25033bb46bb40fe5377f6809b
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-04-15 13:02:33 +0100

    Default retval's set in function and fundecl nodes

--- src/ast/function_node.h
+++ src/ast/function_node.h
@@ -1,15 +1,16 @@
-// $Id: function_node.h,v 1.11 2017/04/13 11:10:23 ist181045 Exp $ -*- c++ -*-
+// $Id: function_node.h,v 1.12 2017/04/15 12:02:29 ist181045 Exp $ -*- c++ -*-
 #ifndef __XPL_FUNCTIONNODE_H__
 #define __XPL_FUNCTIONNODE_H__
 
 #include <cdk/ast/basic_node.h>
-#include <cdk/ast/literal_node.h>
+#include <cdk/ast/expression_node.h>
 #include <cdk/ast/sequence_node.h>
 #include <cdk/basic_type.h>
 
 #include <string>
 
 #include "block_node.h"
+#include "null_node.h"
 
 namespace xpl {
 
@@ -18,15 +19,25 @@ namespace xpl {
     basic_type *_type;
     std::string *_name;
     cdk::sequence_node *_arguments;
-    cdk::literal_node<basic_type> *_retval;
     block_node *_body;
+    cdk::expression_node *_retval;
 
   public:
     inline function_node(int lineno, int scope, basic_type *type,
-        std::string *name, cdk::sequence_node *arguments,
-        cdk::literal_node<basic_type> *retval, block_node *body)
+        std::string *name, cdk::sequence_node *arguments, block_node *body,
+        cdk::expression_node *retval = nullptr)
         : cdk::basic_node(lineno), _scope(scope), _type(type), _name(name),
-        _arguments(arguments), _retval(retval), _body(body) {
+        _arguments(arguments), _body(body), _retval(retval) {
+      if (retval == nullptr) {
+        if (type->name() == basic_type::TYPE_INT)
+          _retval = new cdk::integer_node(lineno, 0);
+        else if (type->name() == basic_type::TYPE_POINTER)
+          _retval = new null_node(lineno);
+      }
+    }
+
+    ~function_node() {
+      if (_retval) delete _retval;
     }
 
   public:
@@ -36,17 +47,17 @@ namespace xpl {
     inline basic_type *type() {
       return _type;
     }
-    inline std::string *name() {
-      return _name;
+    inline const std::string &name() const {
+      return *_name;
     }
     inline cdk::sequence_node *arguments() {
       return _arguments;
     }
-    inline cdk::literal_node<basic_type> *retval() {
-      return _retval;
-    }
     inline block_node *body() {
       return _body;
+    }
+    inline cdk::expression_node *retval() {
+      return _retval;
     }
 
     void accept(basic_ast_visitor *sp, int level) {
--- src/ast/fundecl_node.h
+++ src/ast/fundecl_node.h
@@ -1,4 +1,4 @@
-// $Id: fundecl_node.h,v 1.4 2017/04/14 16:42:48 ist181045 Exp $ -*- c++ -*-
+// $Id: fundecl_node.h,v 1.5 2017/04/15 12:02:29 ist181045 Exp $ -*- c++ -*-
 #ifndef __XPL_FUNDECLNODE_H__
 #define __XPL_FUNDECLNODE_H__
 
@@ -15,12 +15,24 @@ namespace xpl {
     basic_type *_type;
     std::string *_name;
     cdk::sequence_node *_arguments;
+    cdk::expression_node *_retval;
 
   public:
-    inline fundecl_node(int lineno, int scope, basic_type *type, std::string *name,
-        cdk::sequence_node *arguments)
+    inline fundecl_node(int lineno, int scope, basic_type *type,
+        std::string *name, cdk::sequence_node *arguments,
+        cdk::expression_node *retval = nullptr)
         : cdk::basic_node(lineno), _scope(scope), _type(type), _name(name),
-        _arguments(arguments) {
+        _arguments(arguments), _retval(retval) {
+      if (retval == nullptr) {
+        if (type->name() == basic_type::TYPE_INT)
+          _retval = new cdk::integer_node(lineno, 0);
+        else if (type->name() == basic_type::TYPE_POINTER)
+          _retval = new null_node(lineno);
+      }
+    }
+
+    ~fundecl_node() {
+      if (_retval) delete _retval;
     }
 
   public:
@@ -30,11 +42,14 @@ namespace xpl {
     inline basic_type *type() {
       return _type;
     }
-    inline std::string *name() {
-      return _name;
+    inline const std::string &name() const {
+      return *_name;
     }
     inline cdk::sequence_node *arguments() {
       return _arguments;
+    }
+    inline cdk::expression_node *retval() {
+      return _retval;
     }
 
     void accept(basic_ast_visitor *sp, int level) {
