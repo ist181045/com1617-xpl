7fbb066d2091dfd223a01744a76c6cfc62c240b1
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-04-15 13:04:17 +0100

    Add newline bool to print node

--- src/ast/print_node.h
+++ src/ast/print_node.h
@@ -1,4 +1,4 @@
-// $Id: print_node.h,v 1.3 2017/04/11 22:45:15 ist181045 Exp $ -*- c++ -*-
+// $Id: print_node.h,v 1.4 2017/04/15 12:04:13 ist181045 Exp $ -*- c++ -*-
 #ifndef __XPL_PRINTNODE_H__
 #define __XPL_PRINTNODE_H__
 
@@ -9,13 +9,18 @@ namespace xpl {
 
   class print_node: public cdk::basic_node {
     cdk::expression_node *_argument;
+    bool _newline;
 
   public:
-    inline print_node(int lineno, cdk::expression_node *argument)
-        : cdk::basic_node(lineno), _argument(argument) {
+    inline print_node(int lineno, cdk::expression_node *argument,
+        bool newline = false)
+        : cdk::basic_node(lineno), _argument(argument), _newline(newline) {
     }
 
   public:
+    inline const bool newline() const {
+      return _newline;
+    }
     inline cdk::expression_node *argument() {
       return _argument;
     }
