5577a07e1dfd0a3f9f6cf2703d6aaf59901177d8
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-05-20 19:19:18 +0100

    Implement type checking for unary expressions

--- src/targets/type_checker.cpp
+++ src/targets/type_checker.cpp
@@ -6,47 +6,109 @@
     { if (node->type() != nullptr && \
           node->type()->name() != basic_type::TYPE_UNSPEC) return; }
 
-//---------------------------------------------------------------------------
+/** Handy helpers */
+/* Creates and returns a basic_type* from a given type */
+inline basic_type * create_type(basic_type * const type) {
+  basic_type * new_type = new basic_type(type->size(), type->name());
+  if (type->subtype())
+    new_type->_subtype = create_type(type->subtype());
+  return new_type;
+}
 
+/* If the given type is unspecified, default it to int */
+inline void default_if_unspec(basic_type * const type) {
+  if (type->name() == basic_type::TYPE_UNSPEC) {
+    type->_size = 4;
+    type->_name = basic_type::TYPE_INT;
+  }
+}
+
+//===========================================================================
+
+/** Sequence */
 void xpl::type_checker::do_sequence_node(cdk::sequence_node * const node, int lvl) {
   for (size_t ix = 0; ix < node->size(); ++ix)
     node->node(ix)->accept(this, lvl);
 }
 
-//---------------------------------------------------------------------------
+//===========================================================================
 
+/** Literals */
+/* Integer */
 void xpl::type_checker::do_integer_node(cdk::integer_node * const node, int lvl) {
   ASSERT_UNSPEC;
   node->type(new basic_type(4, basic_type::TYPE_INT));
 }
 
+/* Real (NOT DOUBLE) */
 void xpl::type_checker::do_double_node(cdk::double_node * const node, int lvl) {
   ASSERT_UNSPEC;
   node->type(new basic_type(8, basic_type::TYPE_DOUBLE));
 }
 
+/* String */
 void xpl::type_checker::do_string_node(cdk::string_node * const node, int lvl) {
   ASSERT_UNSPEC;
   node->type(new basic_type(4, basic_type::TYPE_STRING));
 }
 
-//---------------------------------------------------------------------------
+//===========================================================================
 
+/** Unary expressions */
 inline void xpl::type_checker::processUnaryExpression(cdk::unary_expression_node * const node, int lvl) {
+  ASSERT_UNSPEC;
   node->argument()->accept(this, lvl + 2);
-  if (node->argument()->type()->name() != basic_type::TYPE_INT)
-    throw std::string("wrong type in argument of unary expression");
-
-  // in XPL, expressions are always int
-  node->type(new basic_type(4, basic_type::TYPE_INT));
+  default_if_unspec(node->argument()->type());
+  
+  switch (node->argument()->type()->name()) {
+    case basic_type::TYPE_INT:
+    case basic_type::TYPE_DOUBLE:
+      break;
+    default:
+      throw std::string("wrong type in argument of unary expression");
+  }
+  
+  node->type(create_type(node->argument()->type()));
 }
 
+/* Identity (+expr) */
+void xpl::type_checker::do_identity_node(xpl::identity_node * const node, int lvl) {
+  processUnaryExpression(node, lvl);
+}
+
+/* Negation (-expr) */
 void xpl::type_checker::do_neg_node(cdk::neg_node * const node, int lvl) {
   processUnaryExpression(node, lvl);
 }
 
+/* Logical not (~expr) */
 void xpl::type_checker::do_not_node(cdk::not_node * const node, int lvl) {
   processUnaryExpression(node, lvl);
+  if (node->type()->name() != basic_type::TYPE_INT)
+    throw std::string("wrong type in argument of 'not' expression");
+  node->type(new basic_type(4, basic_type::TYPE_INT));
+}
+
+//---------------------------------------------------------------------------
+
+/* Address of (lval?) */
+void xpl::type_checker::do_address_of_node(xpl::address_of_node * const node, int lvl) {
+  ASSERT_UNSPEC;
+  node->argument()->accept(this, lvl + 2);
+  node->type(new basic_type(4, basic_type::TYPE_POINTER));
+  node->type->_subtype = create_type(node->argument()->type());
+}
+
+/* Memory allocation ([expr]) */
+void xpl::type_checker::do_malloc_node(xpl::malloc_node * const node, int lvl) {
+  ASSERT_UNSPEC;
+  node->expression()->accept(this, lvl + 2);
+  if (node->expression()->type()->name() != basic_type::TYPE_INT)
+    throw std::string("wrong type in argument of malloc expression");
+
+  node->type(new basic_type(4, basic_type::TYPE_POINTER));
+  // context dependent
+  node->type()->_subtype = new basic_type(0, basic_type::TYPE_UNSPEC);
 }
 
 //---------------------------------------------------------------------------
@@ -217,13 +279,6 @@ void xpl::type_checker::do_fundecl_node(xpl::fundecl_node * const node, int lvl)
 
 //---------------------------------------------------------------------------
 
-void xpl::type_checker::do_malloc_node(xpl::malloc_node * const node, int lvl) {
-  ASSERT_UNSPEC;
-  node->expression()->accept(this, lvl + 2);
-}
-
-//---------------------------------------------------------------------------
-
 void xpl::type_checker::do_index_node(xpl::index_node * const node, int lvl) {
   ASSERT_UNSPEC;
   node->expression()->accept(this, lvl + 2);
@@ -239,14 +294,4 @@ void xpl::type_checker::do_null_node(xpl::null_node * const node, int lvl) {
 
 //---------------------------------------------------------------------------
 
-void xpl::type_checker::do_identity_node(xpl::identity_node * const node, int lvl) {
-  ASSERT_UNSPEC;
-
-}
-
 void xpl::type_checker::do_funcall_node(xpl::funcall_node * const node, int lvl) {}
-
-void xpl::type_checker::do_address_of_node(xpl::address_of_node * const node, int lvl) {
-  ASSERT_UNSPEC;
-  node->argument()->accept(this, lvl + 2);
-}
