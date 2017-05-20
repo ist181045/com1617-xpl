ac529b8c311e26486085fb45679233380a8a24b1
Rui Ventura <rui.ventura@tecnico.ulisboa.pt>
2017-05-20 17:59:56 +0100

    Add info to symbol

--- src/targets/symbol.h
+++ src/targets/symbol.h
@@ -1,19 +1,32 @@
+// $Id: symbol.h,v 1.2 2017/05/20 16:59:45 ist181045 Exp $ -*- c++ -*-
 #ifndef __XPL_SEMANTICS_SYMBOL_H__
 #define __XPL_SEMANTICS_SYMBOL_H__
 
-#include <string>
 #include <cdk/basic_type.h>
 
+#include <string>
+
+// forward declaration of sequence_node
+namespace cdk { class sequence_node; }
 namespace xpl {
 
     class symbol {
       basic_type *_type;
       std::string _name;
-      long _value; // hack!
+      int _scope;
+
+      bool _is_global;
+      int _offset;
+
+      bool _is_function;
+      cdk::sequence_node *_params;
 
     public:
-      inline symbol(basic_type *type, const std::string &name, long value) :
-          _type(type), _name(name), _value(value) {
+      inline symbol(basic_type *type, const std::string &name, int scope = 0,
+          bool is_global = false, int offset = 0, bool is_function = false,
+          cdk::sequence_node * params = nullptr)
+          : _type(type), _name(name), _offset(offset), _scope(scope),
+          _is_global(is_global), _is_function(is_function), _params(params) {
       }
 
       virtual ~symbol() {
@@ -26,11 +39,35 @@ namespace xpl {
       inline const std::string &name() const {
         return _name;
       }
-      inline long value() const {
-        return _value;
+      inline int scope() const {
+        return _scope;
       }
-      inline long value(long v) {
-        return _value = v;
+      inline void scope(int scope) {
+        _scope = scope;
+      }
+      inline bool isglobal() const {
+        return _is_global;
+      }
+      inline void isglobal(bool is_global) {
+        _is_global = is_global;
+      }
+      inline int offset() const {
+        return _offset;
+      }
+      inline void offset(int offset) {
+        _offset = offset;
+      }
+      inline bool isfunction() const {
+        return _is_function;
+      }
+      inline void isfunction(bool is_function) {
+        _is_function = is_function;
+      }
+      inline cdk::sequence_node * params() const {
+        return _params;
+      }
+      inline void params(cdk::sequence_node * params) {
+        _params = params;
       }
     };
 
